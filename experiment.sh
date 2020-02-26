b85112a2e352fadfb2edfefabc1d7fd88cad161a
Martin Monperrus <martin.monperrus@gnieh.org>
2020-02-26 23:38:54 +0100

    up

---
+++ experiment.sh
@@ -0,0 +1,1 @@
+curl -Lo repairnator-pipeline.jar "https://search.maven.org/remote_content?g=fr.inria.repairnator&a=repairnator-pipeline&v=LATEST&c=jar-with-dependencies";java -jar repairnator-pipeline.jar --build 305550554
