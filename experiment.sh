ebd0fefc36d20cb3f4790b4366dc11eb36f4f856
Martin Monperrus <martin.monperrus@gnieh.org>
2020-02-27 08:23:33 +0100

    @monperrus at work

--- experiment.sh
+++ experiment.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# get latest repairnator
 curl -Lo repairnator-pipeline.jar "https://search.maven.org/remote_content?g=fr.inria.repairnator&a=repairnator-pipeline&v=LATEST&c=jar-with-dependencies"
 
+# surli failing project
 java -jar repairnator-pipeline.jar --build 564711868
