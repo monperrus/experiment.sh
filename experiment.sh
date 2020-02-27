7fe69a5960eb8f5d9792a6a1b30b4c9473a2ce37
Martin Monperrus <martin.monperrus@gnieh.org>
2020-02-27 10:05:29 +0100

    up

--- experiment.sh
+++ experiment.sh
@@ -5,3 +5,5 @@ curl -Lo repairnator-pipeline.jar "https://search.maven.org/remote_content?g=fr.
 
 # surli failing project
 java -jar repairnator-pipeline.jar --build 564711868
+curl -Lo repairnator-pipeline.jar "https://search.maven.org/remote_content?g=fr.inria.repairnator&a=repairnator-pipeline&v=LATEST&c=jar-with-dependencies"
+java -jar repairnator-pipeline.jar --build 652896754
